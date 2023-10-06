e6f5ed1b1d541fcc5c3064eae6f3e2302d148330
thatsachin <138992255+thatsachin@users.noreply.github.com>
2023-10-06 14:20:57 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -59,7 +59,7 @@ const Login = () => {
       try {
         setButtonText('Loading...');
         setLoading(true);
-        const res = await axios.post("http://localhost:5000/api/v1/auth/login", {
+        const res = await axios.post("https://url-shortener-backend-szjh.onrender.com/api/v1/auth/login", {
             email,
             password
         }, {
